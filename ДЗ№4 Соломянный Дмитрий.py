a9fbd47bb07f688760fef9421c8298a04e6b00ca
Dmytro-pytho <dmitry.solomiannyi@gmail.com>
2020-11-30 22:29:17 +0200

    completed two tasks

---
+++ ДЗ№4 Соломянный Дмитрий.py
@@ -0,0 +1,68 @@
+# Задание №1
+# my_list = [50,101,120,99,75,199,200]
+# for number in my_list:
+#     if number>100:
+#         print("100 <", number)
+################################################
+# Задание №2
+my_list = [50,101,120,99,75,199,200,350]
+my_new_list = []
+for index in my_list:
+    if index>100:
+        my_new_list.append(index)
+print (my_new_list)
+################################################
+# Задание №3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# my_list = ["a","b","c","d","e","f"]
+# my_str = "".join(my_list[::2])
+# print(my_str)
+#
+
+
+
+# values = [1,2,3,4,5]
+# new_value = values.copy()
+# new_value.append(6)
+# print(values)
+# # print(new_value)
+
+
+
+
+# values = [1,2,3,4,5]
+# result = []
+# for value in values[::-1]:
+#     result.append(value)
+# print(result)
+
+# values = tuple(values)
+# print(type(values))
+
+
+# my_tuple = (1, 2, 3, 4, 5, 6, "a", 8, 9)
+#
+# index = -6
+#
+# print(my_tuple[index])
