995db3b0def08f42904b20082cdbf988013f6ded
Dmytro-pytho <dmitry.solomiannyi@gmail.com>
2020-12-06 00:02:17 +0200

    completed all tasks

---
+++ Home_task_#5.py
@@ -0,0 +1,46 @@
+# Задание №1
+# my_int = 123450505608902000222020
+# new_type = str(my_int)
+# search_number = "0"
+# zero = new_type.count(search_number)
+# print("Количество нулей в числе равно = "
+# zero)
+# ########################################
+#
+# Задание №2
+# my_int = 5076070000000
+# new_number = str(my_int)
+# result = len(new_number) - len(new_number.strip("0"))
+# print("Количесвто нулей в конце числа равно = ", result)
+# ########################################
+#
+# Задание №3a
+
+# my_list1 = [1, 2, 3, 4, 5]
+# my_list2 = [10, 15, 20, 25]
+# odd_index = my_list1[::2]
+# even_index = my_list2[1::2]
+# new_list = odd_index + even_index
+# print(new_list)
+
+# Задание №3б
+# my_list1 = [1, 2, 3, 4, 5]
+# my_list2 = [10, 15, 20, 25]
+# new_list = []
+# for index in range(len(my_list1)):
+#     if index % 2:
+#         new_list.append(my_list1[index])
+# for index in range(len(my_list2)):
+#     if not index % 2 == 0:
+#         new_list.append(my_list2[index])
+# print(new_list)
+# # ########################################
+
+# Задание №4
+my_list = [1, 2, 3, 4]
+new_list = [2, 3, 4, 1]
+my_list = new_list
+print(new_list)
+####################################
+
+# Задание №5
--- hello world.py
+++ hello world.py
@@ -1,1 +1,4 @@
-print(5==5.0)
+my_int = 5076070000000
+new_number = str(my_int)
+result = len(new_number) - len(new_number.rstrip('0'))
+print(result)
