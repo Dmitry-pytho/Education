ff39dc0ca84703f60390c45441e1b3f0f323b741
Dmytro-pytho <dmitry.solomiannyi@gmail.com>
2020-12-08 19:04:18 +0200

    completed 3b task

--- Home_task_#5.py
+++ Home_task_#5.py
@@ -25,7 +25,16 @@ for number in range(1, len(my_list2), 2):
 print(new_list)
 
 #Задание №3б
-
+my_list1 = [1, 2, 3, 4, 2, 4, 6]
+my_list2 = [10, 15, 20, 25, 21, 23]
+new_list = []
+for element in my_list1:
+    if element % 2 == 0:
+        new_list.append(element)
+for element in my_list2:
+    if element % 2 != 0:
+        new_list.append(element)
+print(new_list)
 #########################################
 
 # Задание №4
--- hello world.py
+++ hello world.py
@@ -1,10 +1,10 @@
-my_list1 = [1, 2, 3, 4]
-my_list2 = [10, 15, 20, 25]
+my_list1 = [1, 2, 3, 4, 2, 4, 6]
+my_list2 = [10, 15, 20, 25, 21, 23]
 new_list = []
-for index in range(len(my_list1)):
-    if index % 2:
-        new_list.append(my_list1[index])
-for index in range(len(my_list2)):
-    if  index % 2 != 0:
-        new_list.append(my_list2[index])
+for element in my_list1:
+    if element % 2 == 0:
+        new_list.append(element)
+for element in my_list2:
+    if element % 2 != 0:
+        new_list.append(element)
 print(new_list)
