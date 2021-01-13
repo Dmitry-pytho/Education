8d6a1d0bdee2bf518046a87c9c55a57db5da65e6
Dmytro-pytho <dmitry.solomiannyi@gmail.com>
2021-01-13 12:12:24 +0200

    completed all def

--- Home_task#11.py
+++ Home_task#11.py
@@ -17,16 +17,31 @@ def key_sorted_by_name(obj_dict):
 
 
 name_dict_list = sorted(content_file, key=key_sorted_by_name)
-print(*name_dict_list, sep="\n")
+# print(name_dict_list)
 
 
 
-# def key_sorted_by_dates_of_life(obj_dict):
-#     dates_of_life = obj_dict["years"].split("-")[1]
-#     print(dates_of_life)
-#     if len(dates_of_life.split()) ==1:
-#         return dates_of_life.split()[0][:-1]
-#     return -int(dates_of_life.split(" ")[-2])
-#
-# dates_of_life_dict_list = sorted(content_file, key=key_sorted_by_dates_of_life)
-# print(dates_of_life_dict_list)
+def key_sorted_by_dates_of_life(obj_dict):
+    death_date = obj_dict["years"].split(" – ")[1]
+    year = ''
+    for liter in death_date:
+        if liter.isdigit():
+            year += liter
+    if "BC" in death_date:
+        return -int(year)
+    return int(year)
+
+
+dates_of_life_dict_list = sorted(content_file, key=key_sorted_by_dates_of_life)
+# print(*dates_of_life_dict_list, sep="\n")
+
+def key_sorted_by_quantity_words_in_text(obj_dict):
+    quantity_words = len(obj_dict["text"].split())
+    return quantity_words
+
+quantity_words_dict_list = sorted(content_file, key=key_sorted_by_quantity_words_in_text)
+# print(*quantity_words_dict_list, sep="\n")
+
+
+
+
