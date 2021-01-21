f644ecc33e17d5301a0f7fe716dc5c822964ad59
Dmitry-pytho <75211476+Dmitry-pytho@users.noreply.github.com>
2021-01-21 23:01:14 +0200

    completed

---
+++ Home_task#13.py
@@ -0,0 +1,53 @@
+from random import randint, choice, shuffle
+import string
+
+
+class EmailGeneratror:
+
+    def __init__(self, names, domains):
+        self.name_txt = names
+        self.domains_txt = domains
+
+        self.domains = self.get_domain()
+        self.names = self.get_name()
+
+    def get_domain(self):
+        with open(self.domains_txt, "r") as txt:
+            spisok_domenov = []
+            for line in txt.readlines():
+                spisok_domenov.append(line.replace("\n", "").replace(".", ""))
+            random_domen = choice(spisok_domenov)
+            return random_domen
+
+    def get_name(self):
+        with open(self.name_txt, "r") as text:
+            spisok_names = []
+            for line in text.readlines():
+                spisok_names.append(line.split()[1])
+            random_name = choice(spisok_names)
+            return random_name
+
+    def generate_random_numbers(self):
+        self.number = str(randint(100, 1000))
+        return self.number
+
+    def generate_random_letters(self):
+        alphabet = list(string.ascii_lowercase)
+        shuffle(alphabet)
+        random_letters = ""
+        for x in range(randint(5, 7)):
+            letter = choice(alphabet)
+            random_letters = random_letters + letter
+        return random_letters
+
+    def generate_email(self):
+        result = self.get_name() + self.generate_random_numbers() + "@" + self.generate_random_letters() + "." + self.get_domain()
+        return result
+
+
+email_generator = EmailGeneratror("d:\Дима\Python\Education\_names.txt", "d:\Дима\Python\Education\domains.txt")
+
+print(len(email_generator.names), len(email_generator.domains))
+
+email = email_generator.generate_email()
+print(email)
